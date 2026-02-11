66150b12bbf547af43ffd8f81ec8e82fbd345809
Tim3208 <joungou.park@gmail.com>
2026-02-11 13:02:07 +0900

    feat: 모집 일정 섹션 추가

---
+++ features/public/notice/NoticePage.tsx
@@ -0,0 +1,9 @@
+import ScheduleSection from "./sections/ScheduleSection";
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col">
+      <ScheduleSection />
+    </div>
+  );
+}
