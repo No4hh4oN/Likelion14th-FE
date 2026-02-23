cccb82fd6409c88d15be9a3da2a6fa2f1fdc90cd
Tim3208 <joungou.park@gmail.com>
2026-02-23 22:46:22 +0900

    feat: ActionButton hoverClass prop 추가

--- features/public/mypage/components/ActionButton.tsx
+++ features/public/mypage/components/ActionButton.tsx
@@ -5,6 +5,7 @@ type ActionButtonProps = {
   text: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
   className?: string;
+  hoverClassName?: string;
   type?: "button" | "submit" | "reset";
   disabled?: boolean;
 };
@@ -13,6 +14,7 @@ type ActionButtonProps = {
  * @param {string} text: 버튼에 표시될 텍스트
  * @param {MouseEventHandler<HTMLButtonElement>} onClick: 버튼 클릭 시 실행될 함수
  * @param {string} className: 버튼의 추가적인 클래스
+ * @param {string} hoverClassName: hover시 추가적인 클래스 (hover:class 형식으로 입력해주어야 함) (기본값: hover:bg-gray-5)
  * @param {"button" | "submit" | "reset"} type: 버튼의 타입 (기본값: "button")
  * @param {boolean} disabled: 버튼 비활성화 여부 (기본값: false)
  */
@@ -20,6 +22,7 @@ export default function ActionButton({
   text,
   onClick,
   className,
+  hoverClassName = "hover:bg-gray-5",
   type = "button",
   disabled = false,
 }: ActionButtonProps) {
@@ -29,7 +32,8 @@ export default function ActionButton({
       onClick={onClick}
       disabled={disabled}
       className={clsx(
-        "rounded-[5px] bg-gray-6 text-[16px] px-7 py-1.75 font-normal text-white-1 transition-colors hover:bg-gray-5",
+        "rounded-[5px] cursor-pointer bg-gray-6 px-7 py-1.75 text-[16px] font-normal text-white-1 transition-colors",
+        hoverClassName,
         className,
       )}
     >
